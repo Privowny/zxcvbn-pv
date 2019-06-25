1733517a463855086a2b6582f08a634ef31d18f6
Daniel Glazman <daniel@privowny.com>
2019-06-25 10:29:07 +0200

    comments added

--- src/haveibeenpwned.js
+++ src/haveibeenpwned.js
@@ -1,14 +1,25 @@
+/*
+ * @CLASS Helper class for HaveIBeenPwned
+ */
 class HaveIBeenPwned {
 
+  // HaveIBeenPwned password API
+  // Cf. https://haveibeenpwned.com/API/v2#PwnedPasswords
   get URL() { return "https://api.pwnedpasswords.com/range/"; }
 
-  request(obj) {
+  /*
+   * Helper for XMLHttpRequest.
+   *
+   * @param {Object} aObject - url (string), method (string), headers (string[]), body (any)
+   * @returns {Promise}
+   */
+  request(aObj) {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
-        xhr.open(obj.method || "GET", obj.url);
-        if (obj.headers) {
-            Object.keys(obj.headers).forEach(key => {
-                xhr.setRequestHeader(key, obj.headers[key]);
+        xhr.open(aObj.method || "GET", aObj.url);
+        if (aObj.headers) {
+            Object.keys(aObj.headers).forEach(key => {
+                xhr.setRequestHeader(key, aObj.headers[key]);
             });
         }
         xhr.onload = () => {
@@ -19,38 +30,54 @@ class HaveIBeenPwned {
             }
         };
         xhr.onerror = () => reject(xhr.statusText);
-        xhr.send(obj.body);
+        xhr.send(aObj.body);
     });
-};
+  };
 
-
+  /*
+   * Checks a password against HaveIBeenPwned. Async.
+   *
+   * @param {string} aPassword - the password to check
+   * @returns {Object}
+   */
   async check(aPassword) {
-    const sha1 = await this.digestSha1(aPassword);
-    const url = this.URL + sha1.toUpperCase().substr(0, 5);
-    const sha1Suffix = sha1.toUpperCase().substr(5);
+    // compute SHA-1 digest
+    const sha1 = (await this.digestSha1(aPassword)).toUpperCase();
+    // form API url with first 5 chars of SHA-1 digest
+    const url = this.URL + sha1.substr(0, 5);
+    // preserve the rest
+    const sha1Suffix = sha1.substr(5);
 
     let rv = await this.request({url: url})
-              .then((aPwnedList) => {
-                const responseArray = aPwnedList.split("\r\n")
-                                      .map(aValue => aValue.split(":"));
-                const pwned = responseArray.some(aEntry => aEntry[0] === sha1Suffix);
-                if (pwned) { 
-                  return {
-                    password: aPassword,
-                    score: -1,
-                    feedback: { warning: "leaked", suggestions: ["leaked"] }
-                  };
-                }
-              })
-            .catch(error => {
-              console.log(error);
-            });
+      .then((aPwnedList) => {
+        // checks if the SHA-1 suffix is present in the answer
+        const responseArray = aPwnedList.split("\r\n")
+                              .map(aValue => aValue.split(":"));
+        const leaked = responseArray.some(aEntry => aEntry[0] === sha1Suffix);
+        if (leaked) {
+          // it is...
+          return {
+            password: aPassword,
+            score: -1,
+            feedback: { warning: "leaked", suggestions: [] }
+          };
+        }
+      })
+    .catch(error => {
+      console.log(error);
+    });
 
     return rv;
   }
 
-  async digestSha1(str) {
-    const buf = await crypto.subtle.digest("SHA-1", new TextEncoder("utf-8").encode(str));
+  /*
+   * Computes a SHA-1 digest through crypto.subtle builtin browser lib. Async.
+   *
+   * @param {string} aString
+   * @returns {string}
+   */
+  async digestSha1(aString) {
+    const buf = await crypto.subtle.digest("SHA-1", new TextEncoder("utf-8").encode(aString));
     return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
   }
 }
