23381c87709a68caad901175e475a3b71f5b6c46
Daniel Glazman <daniel@privowny.com>
2019-06-25 10:13:26 +0200

    final main with HaveIBeenPwned

--- src/zxcvbn.js
+++ src/zxcvbn.js
@@ -6,35 +6,67 @@ class Zxcvbn {
   /*
    * @constructor
    */
-  constructor() {
+  constructor(aLocale) {
     this.frequencyLists  = new ZxcvnFrequencyLists();
     this.adjacencyGraphs = new ZxcvbnAdjacencyGraphs();
-    this.timeEstimates   = new ZxcvbnTimeEstimates([]);
-    this.scoring         = new ZxcvbnScoring("en", this.adjacencyGraphs);
+    this.L10N            = new ZxcvbnL10N(aLocale);
+    this.timeEstimates   = new ZxcvbnTimeEstimates(this.L10N.locale);
+    this.scoring         = new ZxcvbnScoring(aLocale, this.adjacencyGraphs);
     this.matching        = new ZxcvbnMatching(this.frequencyLists, this.adjacencyGraphs, this.scoring);
     this.feedback        = new ZxcvbnFeedback(this.scoring);
+
+    this.HaveIBeenPwned  = new HaveIBeenPwned();
   }
 
+  /*
+   * Returns the current timestamp
+   *
+   * @returns {number}
+   */
   getTime() {
     return (new Date()).getTime();
   }
 
-  evaluate(aPassword, aSanitizedInputs) {
-    const start = this.getTime();
+  /*
+   * Checks a password against HaveIBeenPwned and if the password is not
+   * leaked, calls zxcvbn. Async.
+   *
+   * @param {string} aPassword - the password to check
+   * @rapam {Array} aSanitizedInputs - an extra array of strings as disctionary
+   */
+  async evaluate(aPassword, aSanitizedInputs) {
+    // check against HaveIBennPwned password API
+    let rv = await this.HaveIBeenPwned.check(aPassword);
 
-    const sanitizedInputs = aSanitizedInputs || [];
-    this.matching.setUserInputDictionary(sanitizedInputs);
+    // check against zxcvbn only if not leaked
+    if (!rv) {
+      // we need time estimates
+      const start = this.getTime();
 
-    const matches = this.matching.omnimatch(aPassword);
-    const rv = this.scoring.mostGuessableMatchSequence(aPassword, matches);
+      // set extra dicts if any
+      const sanitizedInputs = aSanitizedInputs || [];
+      this.matching.setUserInputDictionary(sanitizedInputs);
 
-    result.execTime = this.getTime() - start;
-    const attackTimes = this.timeEstimates.estimateAttackTimes(rv.guesses);
-    for (let prop in attackTimes) {
-      rv[prop] = attackTimes[prop];
+      // call zxcvbn matching
+      const matches = this.matching.omnimatch(aPassword);
+      // then scoring
+      rv = this.scoring.mostGuessableMatchSequence(aPassword, matches);
+
+      // compute attack times
+      rv.execTime = this.getTime() - start;
+      const attackTimes = this.timeEstimates.estimateAttackTimes(rv.guesses);
+      for (let prop in attackTimes) {
+        rv[prop] = attackTimes[prop];
+      }
+
+      // gather feedback
+      rv.feedback = this.feedback.getFeedback(rv.score, rv.sequence);
     }
 
-    rv.feedback = this.feedback.getFeedback(rv.score, rv.sequence);
+    // localize warning and suggestions
+    rv.feedback.warning = this.L10N.localize(rv.feedback.warning);
+    for (let i = 0; i < rv.feedback.suggestions.length; i++)
+      rv.feedback.suggestions[i] = this.L10N.localize(rv.feedback.suggestions[i]);
 
     return rv;
   }
